75ae22365352fd4bbaa634e3cc53b14fb360b0a8
Victor Costa <vfcosta@gmail.com>
2015-05-24 19:00:28 -0300

    Compute the inverse of a matrix using a cache strategy

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Define functions to compute the inverse of a matrix using a 
+## cache strategy to avoid extra computational cost.
 
-## Write a short comment describing this function
+## Create a matrix that store its inverse result.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(new_inverse) inverse <<- new_inverse
+    getinverse <- function() inverse
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'.
+## Solve the inverse of a matrix only in the first call.
+## The following calls will use the cached result.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getinverse()
+    if(!is.null(inverse)) {
+        message("getting cached data")
+        return(inverse)
+    }
+    data <- x$get()
+    inverse <- solve(data, ...)
+    x$setinverse(inverse)
+    inverse
 }
